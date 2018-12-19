faadf2b52f9b3318d6dc6fe186fa8a054c9ae8fe
priyaljain919 <priyal.jain@pepcus.com>
2018-12-19 18:33:21 +0530

    added all src files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
+import './dataGrid.css';
 import AppContainer from './components/coreComponents/AppContainer';
 import store from './store/store';
 
