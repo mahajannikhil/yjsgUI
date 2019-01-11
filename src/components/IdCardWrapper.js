d2d4ecd2800f51e06aaa9ce83b89f187de0ccacf
priyalpepcus <45934983+priyalpepcus@users.noreply.github.com>
2019-01-11 20:17:03 +0530

    empty column message validation applied (#19)
    Enhancement:
    Added 'No Column Selected' message on student-search screen

--- src/components/IdCardWrapper.js
+++ src/components/IdCardWrapper.js
@@ -4,20 +4,23 @@ class IdCardWrapper extends Component{
 
   render(){
     return(
-      <div className="buttons-wrapper">
-        <div className="buttonContainer">
-          <button className="linkButton">Export</button>
+        <div className = "id-card-wrapper">
+            <div className="buttons-wrapper">
+                <div className="buttonContainer">
+                    <button className="linkButton">Export</button>
+                </div>
+                <div className="buttonContainer">
+                    <button className="linkButton">Print Now</button>
+                </div>
+                <div className="buttonContainer">
+                    <button className="linkButton">Print Later</button>
+                </div>
+                <div className="buttonContainer">
+                    <button className="linkButton">Present</button>
+                </div>
+            </div>
         </div>
-        <div className="buttonContainer">
-          <button className="linkButton">Print Now</button>
-        </div>
-        <div className="buttonContainer">
-          <button className="linkButton">Print Later</button>
-        </div>
-        <div className="buttonContainer">
-          <button className="linkButton">Present</button>
-        </div>
-      </div>
+
     );
   }
 }
