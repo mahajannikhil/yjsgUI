aabe438d433cdefac8f34220e8ae3d06f59a1160
Bhagyashree Bhoyar <bhagyashree.bhoyar@pepcus.com>
2019-03-07 18:20:38 +0530

    Solved PR comments.

---
+++ src/utils/textConstants.jsx
@@ -0,0 +1,22 @@
+
+export const PLEASE_SELECT_ANY_ONE_TEXT = 'कृपया एक चुनिए . . .';
+
+export const YES_TEXT = 'हाँ';
+
+export const NO_TEXT = 'ना';
+
+export const ID_NUMBER_TEXT = 'आई.डी. नं.';
+
+export const SECRET_CODE_TEXT = 'सीक्रेट कोड';
+
+export const LINK_INFORMATION_OF_EDIT_INFORMATION_TEXT = 'कृपिया अन्य जानकारी बदलने हेतु यहाँ';
+
+export const CLICK_TEXT = 'क्लिक करे|';
+
+export const YOUR_ID_TEXT = 'आपका ID:';
+
+export const YOUR_SECRET_CODE_TEXT = 'आपका सीक्रेट कोड:';
+
+export const PLEASE_SELECT_COLUMNS_TEXT = 'कृपया स्तंभों का चयन करे';
+
+export const UPLOAD_FILE_TEXT = 'कृपिया फाइल को अपलोड करे';
