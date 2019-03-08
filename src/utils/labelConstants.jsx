782c59fc97edfa6c6c93b01f43ace8f4a35b3f97
BhagyashreeBhoyar <45935228+BhagyashreeBhoyar@users.noreply.github.com>
2019-03-08 14:38:09 +0530

    #16 Create separate constants files. (#99)
    * #16 Created separate constants file.

---
+++ src/utils/labelConstants.jsx
@@ -0,0 +1,36 @@
+
+export const PREVIOUS_YEAR_LEVEL_LABEL = 'पूर्व में किये गए धार्मिक अध्ययन का विवरण';
+
+export const IS_OPT_IN_OR_OPT_OUT_2019_LABEL = '2019 के शिविर की स्वीकृति ?';
+
+export const ID_LABEL = 'आई.डी.';
+
+export const NAME_LABEL = 'नाम';
+
+export const FATHER_OR_HUSBAND_NAME_LABEL = 'पिता / पति का नाम';
+
+export const GENDER_LABEL = 'लिंग';
+
+export const AGE_LABEL = 'उम्र';
+
+export const MOBILE_NUMBER_LABEL = 'मोबाइल नं';
+
+export const MOTHER_MOBILE_NUMBER_LABEL = 'मोबाइल नं. ( माता का )';
+
+export const OCCUPATION_LABEL = 'व्यवसाय (युवा वर्ग हेतु)';
+
+export const EDUCATION_LABEL = 'स्कूल शिक्षा';
+
+export const EMAIL_LABEL = 'ई-मेल';
+
+export const ADDRESS_LABEL = 'पूरा पता';
+
+export const BUS_STOP_LABEL = 'बस स्टॉप (कृपया निकटतम बस स्टॉप चुनें)';
+
+export const WHAT_YOU_WANT_TO_STUDY_LABEL = 'आप क्या अध्ययन करना चाहते हैं ?';
+
+export const MARK_SELECTED_STUDENTS_ATTENDANCE_LABEL = 'कृपिया चयनित छात्रो की उपस्तिथि दर्ज करे ';
+
+export const OPT_IN_STATUS_FOR_SELECTED_STUDENTS_LABEL = 'कृपया चयनित छात्रों की शिविर के लिए हाँ या ना दर्ज़ करें';
+
+export const ID_CARD_PRINT_STATUS_FOR_SELECTED_STUDENTS_LABEL = 'कृपिया चयनित छात्रों के पहचान पत्र का print status अद्यतन करे';
