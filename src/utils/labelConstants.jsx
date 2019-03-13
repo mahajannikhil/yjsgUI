fadd8438309fe29a82256c29be0afac606a3dc6f
Priyal Jain <priyal.jain@pepus.com>
2019-03-13 18:25:41 +0530

    constants changed

--- src/utils/labelConstants.jsx
+++ src/utils/labelConstants.jsx
@@ -33,4 +33,4 @@ export const MARK_SELECTED_STUDENTS_ATTENDANCE_LABEL = 'कृपिया च�
 
 export const OPT_IN_STATUS_FOR_SELECTED_STUDENTS_LABEL = 'कृपया चयनित छात्रों की शिविर के लिए हाँ या ना दर्ज़ करें';
 
-export const ID_CARD_PRINT_STATUS_FOR_SELECTED_STUDENTS_LABEL = 'कृपिया चयनित छात्रों के पहचान पत्र का print status अद्यतन करे';
+export const ID_CARD_PRINT_STATUS_FOR_SELECTED_STUDENTS_LABEL = 'कृपया दिए गए IDs को  कार्ड प्रिंट के लिए चयन करें!';
